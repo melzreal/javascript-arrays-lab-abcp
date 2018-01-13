11b05e6558d575aa432dec4c9af2ca5673df49b0
Melissa Real <melissa.real@hotmail.com>
2018-01-13 18:10:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-const app = "I don't do much."
+
+kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(n){
+  kittens.push(n);
+  
+  return kittens;
+  
+}
+
+
+function destructivelyPrependKitten(n){
+   kittens.unshift(n);
+  
+  return kittens;
+  
+}
+
+function destructivelyRemoveLastKitten(){
+   kittens.pop();
+  
+  return kittens;
+  
+}
+
+function destructivelyRemoveFirstKitten(){
+   kittens.shift();
+  
+  return kittens;
+  
+}
+
+function appendKitten(n){
+
+var kittenArr =  [...kittens, n];
+  
+return kittenArr;
+  
+}
+
+function prependKitten(n){
+
+var kittenArr =  [n, ...kittens];
+  
+return kittenArr;
+  
+}
+
+function removeLastKitten(){
+
+var kittenArr =  [...kittens];
+kittenArr.pop();
+  
+return kittenArr;
+  
+}
+
+function removeFirstKitten(){
+
+var kittenArr =  [...kittens];
+kittenArr.shift();
+  
+return kittenArr;
+  
+}
+
